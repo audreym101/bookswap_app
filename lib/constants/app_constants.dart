b433f023ebcdc389b2edf48e58a3e57704b1c908
audreym101 <m.audrey1@alustudent.com>
2025-11-09 05:52:30 -0800

    feat: add app constants for collections and configurations

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,20 @@
+class AppConstants {
+  static const List<String> bookConditions = [
+    'New',
+    'Like New', 
+    'Good',
+    'Used'
+  ];
+  
+  static const String swapPending = 'pending';
+  static const String swapAccepted = 'accepted';
+  static const String swapRejected = 'rejected';
+  
+  static const String booksCollection = 'books';
+  static const String usersCollection = 'users';
+  static const String swapsCollection = 'swaps';
+  static const String messagesCollection = 'messages';
+  
+  static const int maxImageSize = 800;
+  static const int imageQuality = 70;
+}
